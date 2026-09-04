ff6555f587a497897ba0d1929d408d832fa9d558
diana <diana.yarmykh14@list.ru>
2026-09-04 15:09:26 +0700

    add Task_1_1_1

---
+++ Task_1_1_1/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("java")
+    id("jacoco")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:6.0.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required.set(true)
+    }
+}
